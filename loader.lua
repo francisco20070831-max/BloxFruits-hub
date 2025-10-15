ecb336903a0c7ae136bd70d1103c041abf4500ce
francisco20070831-max <francisco20070831@gmail.com>
2025-10-14 22:31:32 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/f6939516-wq/BloxFruits-hub_Version3.0/main/loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/francisco20070831-max/BloxFruits-hub/main/bloxfruits_admin_hubjj.lua"))()
